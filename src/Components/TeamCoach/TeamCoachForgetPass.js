ec85f3c4811de7387827cb70c738da8f1e850e0f
uchihaghost12 <ajinvs21@gmail.com>
2024-06-11 12:30:58 +0530

    kik

--- src/Components/TeamCoach/TeamCoachForgetPass.js
+++ src/Components/TeamCoach/TeamCoachForgetPass.js
@@ -11,28 +11,29 @@ function TeamCoachForgetPass() {
       <div className='TeamCoachForgetPass-img'></div>
     </div>
     <div class="col">
-        <div>
+        <div className='TeamCoachForgetPass-maindiv'>
       <div className='TeamCoachForgetPass-Head'>
-        <h1>Forget Password</h1>
-        <h4>Forget you password to secure your account</h4>
+        <h1 className='TeamCoachForgetPass-Head1'>Forget Password</h1>
+        <h4 className='TeamCoachForgetPass-Head2'>Forget you password to secure your account.</h4>
       </div>
       <div className='TeamCoachForgetPassDiv-1'>
         <div>
-        <label>E-mail</label>
+        <label className='TeamCoachForgetPass-label'>E-mail</label>
         </div>
-        <input className="" type="email" placeholder="Enter your E-mail" name="email"/>
+        <input className="TeamCoachForgetPass-field" type="email" placeholder="Enter your E-mail" name="email"/>
       <div className='TeamCoachForgetPassDiv-1'>
         <div>
-        <label>Enter a new password</label>
+        <label className='TeamCoachForgetPass-label'>Enter a new password</label>
         </div>
-        <input className="" type="password" placeholder="Enter a new password" name="password"/>
+        <input className="TeamCoachForgetPass-field" type="password" placeholder="Enter a new password" name="password"/>
       </div>
       <div className='TeamCoachForgetPassDiv-1'>
         <div>
-        <label>Confirm password</label>
+        <label className='TeamCoachForgetPass-label'>Confirm password</label>
         </div>
-        <input className="" type="email" placeholder="Enter confirm password" name="cpassword"/>
+        <input className="TeamCoachForgetPass-field" type="email" placeholder="Enter confirm password" name="cpassword"/>
       </div>
+      <button className='TeamCoachForgetPass-button'>Reset</button>
       </div>
       </div>
     </div>
