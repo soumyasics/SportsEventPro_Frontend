f6878cc0b0137312c2b2d9a480bf5fe1baa79ea6
PersuesDale <dalerolince@gmail.com>
2024-06-20 17:01:01 +0530

    ViewTeamCoach Completed

--- src/Components/Admin/AdminViewTeamCoach.js
+++ src/Components/Admin/AdminViewTeamCoach.js
@@ -7,21 +7,39 @@ function AdminViewTeamCoach () {
 
         <div className = 'container AdminViewTeamCoachMainDivBG'>
 
-
             <h1 className = 'AdminViewTeamCoach-h1'>Team Coaches List</h1>
             
-            <table className = 'AdminViewTeamCoach-Table'>
+            <table className = 'AdminViewTeamCoach-Table ' >
 
-                <thead className = ' AdminViewTeamCoach-tablehead'>
+                <thead >
 
-                    <td className = 'col-2 AdminViewTeamCoach-tabledata'>Sl</td>
-                    <td className = 'col-2 AdminViewTeamCoach-tabledata'>Coach Name</td>
-                    <td className = 'col-2 AdminViewTeamCoach-tabledata'>Team Name</td>
-                    <td className = 'col-2 AdminViewTeamCoach-tabledata'>Sport</td>
-                    <td className = 'col-2 AdminViewTeamCoach-tabledata'>Team Count</td>
-                    <td className = 'col-2 AdminViewTeamCoach-tabledata'>View</td>
+                    <tr className = ' AdminViewTeamCoach-tableHeadRow'>
 
+                        <td className = 'col-2 AdminViewTeamCoach-tableHeadData'>Sl</td>
+                        <td className = 'col-2 AdminViewTeamCoach-tableHeadData'>Coach Name</td>
+                        <td className = 'col-2 AdminViewTeamCoach-tableHeadData'>Team Name</td>
+                        <td className = 'col-2 AdminViewTeamCoach-tableHeadData'>Sport</td>
+                        <td className = 'col-2 AdminViewTeamCoach-tableHeadData'>Team Count</td>
+                        <td className = 'col-2 AdminViewTeamCoach-tableHeadDataEnd'>View</td>
+
+                    </tr>
                 </thead>
+
+                <tbody>
+
+                    <tr className='AdminViewTeamCoach-tableBodyRow' >
+
+                        <td className='col-2 AdminViewTeamCoach-tableBodyData'>1 {/*Sl No*/}</td>
+                        <td className='col-2 AdminViewTeamCoach-tableBodyData'>Raju {/* Coach Name */}</td>
+                        <td className='col-2 AdminViewTeamCoach-tableBodyData'>The King {/* Team Name */}</td>
+                        <td className='col-2 AdminViewTeamCoach-tableBodyData'>Cricket {/* Sport */}</td>
+                        <td className='col-2 AdminViewTeamCoach-tableBodyData'>11 {/* Team Count */}</td>
+                        <td className='col-2 AdminViewTeamCoach-tableBodyData'x><a href=" " alt=""> View More </a> {/* Coach Name */}</td>
+                            
+
+                    </tr>
+
+                </tbody>
 
             </table>
 
