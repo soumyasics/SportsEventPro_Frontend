303f4b8a872a905845b80e8f17902e29a7aaff5a
soumyasics <soumya.sics@gmail.com>
2024-07-08 17:34:47 +0530

    p

--- src/Components/Organiser/OrganiserDashboardNavBar.js
+++ src/Components/Organiser/OrganiserDashboardNavBar.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './OrganiserDashboardNavBar.css'
 import img from "../../Assets/Frame 18.jpg"
 import img1 from '../../Assets/Search Button.svg'
 import img2 from '../../Assets/frame2.png'
+import axiosInstance from '../Constant/BaseURL'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function OrganiserDashboardNavBar() {
+    const navigate = useNavigate()
 
+
+    const id=localStorage.getItem('organizerId')
+    const [userData, setUserData] = useState({
+        name:'',email:''
+    });
+
+
+    useEffect(() => {
+
+        let res;
+
+
+        axiosInstance.post(`viewOrganizerById/${id}`).then(res => {
+
+            console.log(res);
+
+
+            setUserData(res.data.data);
+            console.log(userData);
+
+        }).catch(err => {
+            console.log(err);
+        })
+
+
+
+    }, [id]);
+    console.log(userData);
 
     
     return (
@@ -47,8 +78,8 @@ function OrganiserDashboardNavBar() {
 
                             <div className='Organiserh1ContainDiv'>
 
-                                <h1 className = 'mt-2 Organiser-h1 '>Lenin{/* Organiser Name */}</h1>
-                                <h1 className = 'Organiser-h1' style={{ marginTop: "-10px" }}>Lenin123@gmail.com</h1>
+                                <h1 className = ' Organiser-h1 'style={{ marginTop: "-3px",marginLeft:"10px" }}>{userData.name}</h1>
+                                <h1 className = 'Organiser-h1' style={{ marginTop: "-1px",marginLeft:"10px" }}>{userData.email}</h1>
 
                             </div>
                         </div>
