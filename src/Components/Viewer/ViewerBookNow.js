5d1d54644df967d452ad4f907d3e85837ea62b24
PersuesDale <67306565+PersuesDale@users.noreply.github.com>
2024-07-13 10:05:19 +0530

    psh

--- src/Components/Viewer/ViewerBookNow.js
+++ src/Components/Viewer/ViewerBookNow.js
@@ -24,7 +24,7 @@ function ViewerBookNow() {
 
                         <img src={img3} alt='' className='ViewerBookNow-img' />
                         <div className='ViewerUpcoming-content-contain-overlay'>
-                            
+
                             <h1 className='ViewerUpcoming-content-contain-h1-1'>Match On</h1>
                             <h1 className='ViewerUpcoming-content-contain-h1-2'>12/12/2024</h1>
 
@@ -36,7 +36,14 @@ function ViewerBookNow() {
 
                         <div className="ViewerBookNow-body-1">
 
-                            
+                            <h1 className='ViewerBookNow-body-h1'>The Eagles Vs The kings</h1>
+
+                            <div style={{ display: "flex", flexDirection: 'row', gap: '8px' }}>
+
+                                <h1 className='ViewerBookNow-body-h1-1'>TVM Junior Sports</h1>
+                                <h1 className='ViewerBookNow-body-h1-2'>Football</h1>{/* event category */}
+
+                            </div>
 
                         </div>
 
